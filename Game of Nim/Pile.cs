26b601340c493f4cb1f95c10a2e1aebccb53e9a7
Emily R. H <Emily R. H@DESKTOP-A30SVH3>
2022-01-11 08:17:12 +0100

    Add project files.

---
+++ Game of Nim/Pile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_of_Nim
+{
+    public class Pile
+    {
+        public int amount;
+        
+        Random ran = new Random();
+
+        public Pile(int MaxValue, bool randomize=true)
+        {
+            amount = (randomize ? ran.Next(1, MaxValue) : MaxValue);
+        }
+    }
+}
---
+++ Game of Nim/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Game_of_Nim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter max size of piles");
+            string temp = Console.ReadLine();
+            int Max = int.Parse(temp);
+
+            var Pile1 =  new Pile(Max);
+            Max -= Pile1.amount;
+
+            var Pile2 = new Pile(Max);
+            Max -= Pile2.amount;
+
+            var Pile3 = new Pile(Max, false);
+
+            Console.WriteLine(Pile1.amount + "/" + Pile2.amount + "/" + Pile3.amount + "/" + Pile1.amount);
+        }
+    }
+}
